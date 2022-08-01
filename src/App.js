fe793f2bf3e8f65a614d771b7f5414ab8158b3d8
RollyLif <relifungula@gmail.com>
2022-08-01 13:34:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import "./styles/index.scss"
 
-function App() {
+const App=()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        {/* path= "*" renvoie quand on entre un chemin non définit*/}
+        <Route path="/*" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Countries.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+
+const Countries = () => {
+const [data, setData] = useState([]);
+const [rangeValue, setRangeValue]=useState(36);
+
+
+useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all")
+    .then((res) => setData(res.data))
+}, []); 
+
+    return (
+        <div className = "countries">
+            <ul className="radio-container">
+                <input type="range" min="1" max="250" defaultValue={rangeValue} onChange={(e)=> setRangeValue(e.target.value)} />
+            </ul>
+            <ul>
+                {
+                    data.slice(0, rangeValue).map((country, index) =>  (
+                    <Card key={index} country={country} />)
+                )}
+            </ul>
+        </div>
+    );
+};
+
+export default Countries
