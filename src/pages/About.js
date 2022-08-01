53d369b0ac483ac10ba1b3396f7be7e2473895fd
RollyLif <relifungula@gmail.com>
2022-08-01 13:52:06 +0200

    ajout de la description dans la l'onglet à propos

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,17 +9,7 @@ const About = () => {
             <Navigation />
             <h1>A propos</h1>
             <br/>
-            <p>Pour Cécile  Duclaux-Monteil le droit de l’environnement est l’ensemble de règles juridiques ayant pour objectif d’assurer l’environnement mondial, donc ce sont des normes qui interdisent la pollution de l’air pour une meilleure santé.
-En deuxième position intervient la déforestation parce que plus nous coupons des arbres il n’y aura pas assez d’air pour que la personne vie en bon et dit forme.
-Pour cet auteur anonyme le droit de l’environnement est un ensemble de règles visant à proteger les éléments vivants et non vivants de la terre et ses processus écologiques, cela veut dire que ce sont des normes qui sont prévus pour combattre les microbes, par exemple la pollution de l’eau 
-donc la doctrine protège traditionnellement des usages tels que la navigation,le commerce et la pèche , elle s’étend maintenant aux valeurs importantes sur le plan écologique , telles que la préservation de l’eau douce, des marécages et des forets riveraines.
-            </p>
-            <br/>
-            <p>
-            Pour Cécile  Duclaux-Monteil le droit de l’environnement est l’ensemble de règles juridiques ayant pour objectif d’assurer l’environnement mondial, donc ce sont des normes qui interdisent la pollution de l’air pour une meilleure santé.
-En deuxième position intervient la déforestation parce que plus nous coupons des arbres il n’y aura pas assez d’air pour que la personne vie en bon et dit forme.
-Pour cet auteur anonyme le droit de l’environnement est un ensemble de règles visant à proteger les éléments vivants et non vivants de la terre et ses processus écologiques, cela veut dire que ce sont des normes qui sont prévus pour combattre les microbes, par exemple la pollution de l’eau 
-donc la doctrine protège traditionnellement des usages tels que la navigation,le commerce et la pèche , elle s’étend maintenant aux valeurs importantes sur le plan écologique , telles que la préservation de l’eau douce, des marécages et des forets riveraines.
+            <p>une application React qui affiche les drapeaux de 250 pays du monde et au survol, on a le nom du pays, sa capitale et le nombre d'habitant
             </p>
         </div>
     );
